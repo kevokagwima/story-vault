b751ebd2f865f401b5bc34ce1545e09c38216dc3
kevin <you@example.com>
2025-04-02 23:49:41 +0300

    updated files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-  SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/storyvault"
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+  # SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/storyvault"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SECRET_KEY = os.environ.get("SECRET_KEY")
